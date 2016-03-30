1f48e63bb40cd472d78329f38776935fa8506c63
Sharan06 <sharan_u@msn.com>
2016-03-30 09:59:00 -0700

    jQuery
    updates

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
-		$("input[type='submit']").on('click', function() {
-   		alert('message sent');
-		});
+	$('#contact-form').on('submit', function(event) {
+   event.preventDefault();
+   if ( $('#message').val() == '' ) {
+      alert('Please type a message.');
+   } else {
+      $('#contact-form').fadeOut( 800 );
+      	$('#result').fadeIn( 500 );
+   }
+	});
 
 });
 
